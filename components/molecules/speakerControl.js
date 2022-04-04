8b6b5e828d2cbaaca52959f80a9825b850ad7fd4
Braden Weber <bweber@celerik.com>
2022-04-04 19:11:28 -0400

    Initial work

--- components/molecules/speakerControl.js
+++ components/molecules/speakerControl.js
@@ -1,11 +1,46 @@
-import React, { useState } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
 import Colors from '../../global/styles/colors';
 import Fonts from '../../global/styles/fonts';
+import { PermissionsAndroid } from "react-native";
+import Recording from "react-native-recording";
+import { LogBox } from 'react-native';
+
+LogBox.ignoreLogs(['new NativeEventEmitter']);
 
 const SpeakerControl = () => {
     const [volume, setVolume] = useState(17);
     const CIRCLES_ON_SCREEN = 18;
+    let listener
+
+    useEffect(() => {
+        const startRecording = async () => {
+            await PermissionsAndroid.requestMultiple([
+                PermissionsAndroid.PERMISSIONS.RECORD_AUDIO,
+            ]);
+
+            Recording.init({
+                bufferSize: 4096,
+                sampleRate: 44100,
+                bitsPerChannel: 16,
+                channelsPerFrame: 1,
+            });
+
+            listener = Recording.addRecordingEventListener((data) =>
+                console.log(data)
+            );
+
+            Recording.start();
+        }
+
+        startRecording();
+
+        return (async () => {
+            // stop recording
+            Recording.stop();
+            listener.remove();
+        });
+    })
 
     return (
         <View style={styles.container}>
